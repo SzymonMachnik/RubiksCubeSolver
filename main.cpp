4f4d283c02204593c20befa1cc4f658d281213f5
Szymon Machnik <szymonmachnik07@gmail.com>
2024-12-08 20:03:32 +0100

    Added prim moves

--- main.cpp
+++ main.cpp
@@ -39,7 +39,7 @@ void printCube(unordered_map<char, vector<vector<char>>> &cube) {
   cout << endl;
 }
 
-// Funkcja obracająca daną ścianę o 90° zgodnie z ruchem zegara
+
 void rotate_face(vector<vector<char>> &face) {
   vector<vector<char>> temp = face;
   for (int i = 0; i < 3; i++) {
@@ -49,9 +49,17 @@ void rotate_face(vector<vector<char>> &face) {
   }
 }
 
-// Ruchy dla poszczególnych ścian
+void rotate_face_prim(vector<vector<char>> &face) {
+  vector<vector<char>> temp = face;
+  for (int i = 0; i < 3; i++) {
+    for (int j = 0; j < 3; j++) {
+      face[2 - j][i] = temp[i][j];
+    }
+  }
+}
+
+
 void move_R(unordered_map<char, vector<vector<char>>> &cube) {
-  // Rotacja krawędzi
   for (int i = 0; i < 3; i++) {
     char temp = cube['U'][i][2];
     cube['U'][i][2] = cube['F'][i][2];
@@ -59,9 +67,20 @@ void move_R(unordered_map<char, vector<vector<char>>> &cube) {
     cube['D'][i][2] = cube['B'][2 - i][0];
     cube['B'][2 - i][0] = temp;
   }
-  // Rotacja całej ściany R
   rotate_face(cube['R']);
 }
+
+void move_R_prim(unordered_map<char, vector<vector<char>>> &cube) {
+  for (int i = 0; i < 3; i++) {
+    char temp = cube['U'][i][2];
+    cube['U'][i][2] = cube['B'][2 - i][0];
+    cube['B'][2 - i][0] = cube['D'][i][2];
+    cube['D'][i][2] = cube['F'][i][2];
+    cube['F'][i][2] = temp;
+  }
+  rotate_face_prim(cube['R']);
+}
+
 
 void move_L(unordered_map<char, vector<vector<char>>> &cube) {
   for (int i = 0; i < 3; i++) {
@@ -74,6 +93,18 @@ void move_L(unordered_map<char, vector<vector<char>>> &cube) {
   rotate_face(cube['L']);
 }
 
+void move_L_prim(unordered_map<char, vector<vector<char>>> &cube) {
+  for (int i = 0; i < 3; i++) {
+    char temp = cube['U'][i][0];
+    cube['U'][i][0] = cube['F'][i][0];
+    cube['F'][i][0] = cube['D'][i][0];
+    cube['D'][i][0] = cube['B'][2 - i][2];
+    cube['B'][2 - i][2] = temp;
+  }
+  rotate_face_prim(cube['L']);
+}
+
+
 void move_F(unordered_map<char, vector<vector<char>>> &cube) {
   for (int i = 0; i < 3; i++) {
     char temp = cube['U'][2][i];
@@ -84,6 +115,18 @@ void move_F(unordered_map<char, vector<vector<char>>> &cube) {
   }
   rotate_face(cube['F']);
 }
+
+void move_F_prim(unordered_map<char, vector<vector<char>>> &cube) {
+  for (int i = 0; i < 3; i++) {
+    char temp = cube['U'][2][i];
+    cube['U'][2][i] = cube['R'][i][0];
+    cube['R'][i][0] = cube['D'][0][2 - i];
+    cube['D'][0][2 - i] = cube['L'][2 - i][2];
+    cube['L'][2 - i][2] = temp;
+  }
+  rotate_face_prim(cube['F']);
+}
+
 
 void move_B(unordered_map<char, vector<vector<char>>> &cube) {
   for (int i = 0; i < 3; i++) {
@@ -96,6 +139,18 @@ void move_B(unordered_map<char, vector<vector<char>>> &cube) {
   rotate_face(cube['B']);
 }
 
+void move_B_prim(unordered_map<char, vector<vector<char>>> &cube) {
+  for (int i = 0; i < 3; i++) {
+    char temp = cube['U'][0][i];
+    cube['U'][0][i] = cube['L'][2 - i][0];
+    cube['L'][2 - i][0] = cube['D'][2][2 - i];
+    cube['D'][2][2 - i] = cube['R'][i][2];
+    cube['R'][i][2] = temp;
+  }
+  rotate_face_prim(cube['B']);
+}
+
+
 void move_U(unordered_map<char, vector<vector<char>>> &cube) {
   for (int i = 0; i < 3; i++) {
     char temp = cube['F'][0][i];
@@ -106,6 +161,18 @@ void move_U(unordered_map<char, vector<vector<char>>> &cube) {
   }
   rotate_face(cube['U']);
 }
+
+void move_U_prim(unordered_map<char, vector<vector<char>>> &cube) {
+  for (int i = 0; i < 3; i++) {
+    char temp = cube['F'][0][i];
+    cube['F'][0][i] = cube['L'][0][i];
+    cube['L'][0][i] = cube['B'][0][i];
+    cube['B'][0][i] = cube['R'][0][i];
+    cube['R'][0][i] = temp;
+  }
+  rotate_face_prim(cube['U']);
+}
+
 
 void move_D(unordered_map<char, vector<vector<char>>> &cube) {
   for (int i = 0; i < 3; i++) {
@@ -118,6 +185,16 @@ void move_D(unordered_map<char, vector<vector<char>>> &cube) {
   rotate_face(cube['D']);
 }
 
+void move_D_prim(unordered_map<char, vector<vector<char>>> &cube) {
+  for (int i = 0; i < 3; i++) {
+    char temp = cube['F'][2][i];
+    cube['F'][2][i] = cube['R'][2][i];
+    cube['R'][2][i] = cube['B'][2][i];
+    cube['B'][2][i] = cube['L'][2][i];
+    cube['L'][2][i] = temp;
+  }
+  rotate_face_prim(cube['D']);
+}
 
 
 int main() {
@@ -149,55 +226,30 @@ int main() {
   };
   
   
-  move_B(cube);
-  move_B(cube);
-  move_B(cube);
-
-  move_R(cube);
-  move_R(cube);
-
-  move_B(cube);
-  move_B(cube);
-  move_B(cube);
-
+  move_L_prim(cube);
   move_D(cube);
-  move_D(cube);
-  move_D(cube);
-
-  move_L(cube);
-
+  move_B_prim(cube);
   move_U(cube);
   move_U(cube);
-
   move_R(cube);
-  move_R(cube);
-
-  move_F(cube);
-  move_F(cube);
-  move_F(cube);
-
-  move_R(cube);
-
-  move_L(cube);
-  move_L(cube);
-
-  move_F(cube);
-
-  move_D(cube);
-  move_D(cube);
-
-  move_F(cube);
-
-  move_U(cube);
-  move_U(cube);
-
-  move_L(cube);
-  move_L(cube);
-
-  move_D(cube);
-  move_D(cube);
-
+  move_U_prim(cube);
   move_B(cube);
+  move_R_prim(cube);
+  move_U(cube);
+  move_U(cube);
+  move_F(cube);
+  move_F(cube);
+  move_U(cube);
+  move_U(cube);
+  move_R(cube);
+  move_R(cube);
+  move_F(cube);
+  move_F(cube);
+  move_R(cube);
+  move_B(cube);
+  move_B(cube);
+  move_R(cube);
+  move_B_prim(cube);
   
 
   printCube(cube);
